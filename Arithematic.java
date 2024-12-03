301b91ffc82fc1753792a1818d8753f1dff8b4bd
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-03 11:24:25 +0530

    Add files via upload

---
+++ Arithematic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Arithematic{
+    public static void main(String args[]){
+        int a=12;
+        int b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}    
