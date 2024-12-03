888e3a3cdc9f18300fddf9b55598d643bbe547d1
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-03 16:05:53 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class a{
+    public static void main(String args[]){
+        System.out.println(true&&true);
+        System.out.println(false||true);
+        System.out.println(!(true));
+    }
+}
