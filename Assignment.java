9c1f7c2ae55674010c68004a6f8f4d77cbb7fd8a
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-03 11:22:56 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Assignment{
+    public static void main(String ards[]){
+        Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    System.out.println(a+=46);
+    System.out.println(a-=9);
+    System.out.println(a*=2);
+
+    
+}
+}
