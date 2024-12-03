d9e057deac47baf9e88ffa1c134741c1aad01d9b
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-03 11:23:37 +0530

    Add files via upload

---
+++ Comparision.java
@@ -0,0 +1,15 @@
+
+public class Comparision{
+    public static void main(String args[]){
+        int a=45;
+        int b=54;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+    }
+}        
+
+
